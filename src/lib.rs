1713d365a42caa9e710b23bc8357cfc652bb7127
MatthewGadsden <matthewjgadsden@gmail.com>
2024-07-15 00:46:57 +1000

    chore: change port

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub fn run() -> Result<Server, std::io::Error> {
 		App::new()
 			.route("/health_check", web::get().to(health_check))
 	})
-	.bind("127.0.0.1:8000")?
+	.bind("127.0.0.1:5000")?
 	.run();
 
 	Ok(server)
