43c1330417097fad46e41d35d88bb8072de63556
MatthewGadsden <matthewjgadsden@gmail.com>
2024-07-18 01:20:16 +1000

    chore: temp handle for clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,12 @@ async fn health_check() -> HttpResponse {
 
 #[derive(serde::Deserialize)]
 struct FormData {
-	_email: String,
-	_name: String,
+	email: String,
+	name: String,
 }
 
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+	print!("{} & {}", form.email, form.name);
 	HttpResponse::Ok().finish()
 }
 
