e9812a53f731267d88d187d0fcb4d5098c10ddbe
MatthewGadsden <matthewjgadsden@gmail.com>
2024-07-27 21:04:41 +1000

    config: use values defined by config files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-	const ADDRESS: &str = "127.0.0.1:5000";
-	let listener = TcpListener::bind(ADDRESS).expect("Failed to bind random port");
+	let configuration = get_configuration().expect("Failed to read configuration.");
 
+	let address = format!("127.0.0.1:{}", configuration.application_port);
+	let listener = TcpListener::bind(address)?;
 	run(listener)?.await
 }
