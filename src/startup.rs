c89157fa5813fdf91ab242747f33a9549bd08bf5
MatthewGadsden <matthewjgadsden@gmail.com>
2024-07-28 23:53:57 +1000

    feat: add connection to request context

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,22 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-	let server = HttpServer::new(|| {
+pub fn run(
+	listener: TcpListener,
+	connection: PgConnection,
+) -> Result<Server, std::io::Error> {
+	let connection = web::Data::new(connection);
+
+	let server = HttpServer::new(move || {
 		App::new()
 			.route("/health_check", web::get().to(health_check))
 			.route("/subscriptions", web::post().to(subscribe))
 			.route("/", web::get().to(health_check))
+			.app_data(connection.clone())
 	})
 	.listen(listener)?
 	.run();
