04af91b338eceff2c265e102daff1510104d3893
MatthewGadsden <matthewjgadsden@gmail.com>
2024-08-01 22:28:53 +1000

    feat: make tests use fresh dbs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,11 @@
 use reqwest;
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use uuid::Uuid;
+use zero2prod::{
+	configuration::{get_configuration, DatabaseSettings},
+	startup::run,
+};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -107,10 +111,10 @@ async fn spawn_app() -> TestApp {
 	let app_address = format!("http://127.0.0.1:{}", port);
 	println!("{}", app_address);
 
-	let configuration = get_configuration().expect("Failed to read configuration");
-	let connection_pool = PgPool::connect(&configuration.database.connection_string())
-		.await
-		.expect("Failed to connect to Postgres.");
+	let mut configuration = get_configuration().expect("Failed to read configuration");
+	configuration.database.database_name = Uuid::new_v4().to_string();
+
+	let connection_pool = configure_database(&configuration.database).await;
 
 	let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
 	let _ = tokio::spawn(server);
@@ -119,4 +123,26 @@ async fn spawn_app() -> TestApp {
 		address: app_address,
 		db_pool: connection_pool,
 	}
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+	let mut connection = PgConnection::connect(&config.connection_string_without_db())
+		.await
+		.expect("Failed to connect to Postgres");
+
+	connection
+		.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+		.await
+		.expect("Failed to create database.");
+
+	let connection_pool = PgPool::connect(&config.connection_string())
+		.await
+		.expect("Failed to connect to Postgres.");
+
+	sqlx::migrate!("./migrations")
+		.run(&connection_pool)
+		.await
+		.expect("Failed to migrate the database");
+
+	connection_pool
 }
